2d7867e6b7ae13fb899ea6b6e5e547564a780cd9
lucasbf1992 <lucas.bf@outlook.com>
2022-07-11 21:24:24 -0300

    Criando as primeiras rotas

--- express/index.js
+++ express/index.js
@@ -1,6 +1,18 @@
 const express = require("express"); //importando o express
 const app = express(); //iniciando o express
 
+app.get('/', function(req, res) {
+    res.send('Bem vindo ao meu site!')
+});
+
+app.get('/blog', function(req, res) {
+    res.send('Bem vindo ao meu blog!')
+});
+
+app.get('/youtube', function(req, res) {
+    res.send('Bem vindo ao meu canal!')
+});
+
 app.listen(4000, function(error){
     if (error) {
         console.log('Ocorreu algum erro')
