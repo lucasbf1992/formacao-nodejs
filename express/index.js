d52096f6e25279a113f46036d10d06b73490d655
lucasbf1992 <lucas.bf@outlook.com>
2022-07-13 18:03:53 -0300

    Recebendo paramentos

--- express/index.js
+++ express/index.js
@@ -5,12 +5,29 @@ app.get('/', function(req, res) {
     res.send('Bem vindo ao meu site!')
 });
 
-app.get('/blog', function(req, res) {
+app.get('/blog/:artigo?', function(req, res) {
+    var artigo = req.params.artigo;
+
+    if (artigo) {
+        res.send("<h2>Artigo: " + artigo + " </h2>")
+        return
+    }
+
     res.send('Bem vindo ao meu blog!')
 });
 
 app.get('/youtube', function(req, res) {
     res.send('Bem vindo ao meu canal!')
+});
+
+app.get('/ola/:nome/:empresa', function(req, res) {
+    //REQ => DADOS ENVIADOS PELO USUARIO
+    //RES => REPOSTA QUE VAI SER ENVIADO AO USUARIO
+
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+
+    res.send("<h1>Oi " + nome + " da empresa: " + empresa + " </h1>");
 });
 
 app.listen(4000, function(error){
